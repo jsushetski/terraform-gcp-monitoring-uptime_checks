bd09a19aafaeed57978ac0b4d8c98c7f5bb96068
Jeff Sushetski <jsushetski@gmail.com>
2021-04-25 00:06:07 -0600

    Added an alert policy config with one condition

---
+++ alert_policy.tf
@@ -0,0 +1,22 @@
+module "alert_policy" {
+  source = "git::https://github.com/jsushetski/terraform-gcp-monitoring-alert_policies.git?ref=main"
+
+  conditions = [
+    {
+      display_name         = var.tcp_port == null ? "HTTP(S) Check on ${var.host}" : "TCP Check of ${var.host} on port ${var.tcp_port}"
+      comparison           = "COMPARISON_GT"
+      duration             = 60
+      filter               = "metric.type=\"monitoring.googleapis.com/uptime_check/check_passed\" resource.type=\"uptime_url\" metric.label.\"check_id\"=\"${google_monitoring_uptime_check_config.uptime_check.uptime_check_id}\""
+      threshold_value      = 1
+      alignment_period     = 1200
+      cross_series_reducer = "REDUCE_COUNT_FALSE"
+      group_by_fields = [
+        "resource.*",
+      ]
+      per_series_aligner = "ALIGN_NEXT_OLDER"
+      trigger = {
+        count = 1
+      }
+    },
+  ]
+}
