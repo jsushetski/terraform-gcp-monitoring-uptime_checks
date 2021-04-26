5a9182b493aaf98fb8d93232e0220e9238f7e337
Jeff Sushetski <jsushetski@gmail.com>
2021-04-25 23:51:58 -0600

    Added notification channel config to alert policy setup.

--- alert_policy.tf
+++ alert_policy.tf
@@ -38,4 +38,6 @@ module "alert_policy" {
   display_name = var.tcp_port == null ? "${var.host} HTTP(S) Availability" : "${var.host} TCP Check"
 
   conditions_threshold = var.check_ssl ? [local.uptime_check, local.ssl_expiry_check, ] : [local.uptime_check, ]
+
+  notification_channels = var.notification_channels
 }
